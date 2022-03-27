4ec52ced5d71305ae063e17113456d7abbbcd896
Arvin-2005 <102425221+Arvin-2005@users.noreply.github.com>
2022-03-27 21:38:34 +0430

    Add files via upload

---
+++ BAZI 1/Form1.Designer.cs
@@ -0,0 +1,421 @@
+﻿namespace BAZI_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.divar1 = new System.Windows.Forms.Label();
+            this.divar3 = new System.Windows.Forms.Label();
+            this.divar4 = new System.Windows.Forms.Label();
+            this.divar2 = new System.Windows.Forms.Label();
+            this.divar13 = new System.Windows.Forms.Label();
+            this.divar9 = new System.Windows.Forms.Label();
+            this.divar8 = new System.Windows.Forms.Label();
+            this.divar5 = new System.Windows.Forms.Label();
+            this.divar14 = new System.Windows.Forms.Label();
+            this.divar15 = new System.Windows.Forms.Label();
+            this.divar10 = new System.Windows.Forms.Label();
+            this.divar6 = new System.Windows.Forms.Label();
+            this.divar7 = new System.Windows.Forms.Label();
+            this.divar12 = new System.Windows.Forms.Label();
+            this.divar11 = new System.Windows.Forms.Label();
+            this.irani2 = new System.Windows.Forms.Label();
+            this.irani3 = new System.Windows.Forms.Label();
+            this.irani1 = new System.Windows.Forms.Label();
+            this.irani4 = new System.Windows.Forms.Label();
+            this.kh1 = new System.Windows.Forms.Label();
+            this.kh2 = new System.Windows.Forms.Label();
+            this.kh3 = new System.Windows.Forms.Label();
+            this.kh4 = new System.Windows.Forms.Label();
+            this.khar2 = new System.Windows.Forms.Label();
+            this.khar1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.lablel1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // divar1
+            // 
+            this.divar1.BackColor = System.Drawing.Color.Transparent;
+            this.divar1.Location = new System.Drawing.Point(12, 40);
+            this.divar1.Name = "divar1";
+            this.divar1.Size = new System.Drawing.Size(31, 352);
+            this.divar1.TabIndex = 0;
+            // 
+            // divar3
+            // 
+            this.divar3.BackColor = System.Drawing.Color.Transparent;
+            this.divar3.Location = new System.Drawing.Point(49, 40);
+            this.divar3.Name = "divar3";
+            this.divar3.Size = new System.Drawing.Size(320, 33);
+            this.divar3.TabIndex = 1;
+            // 
+            // divar4
+            // 
+            this.divar4.BackColor = System.Drawing.Color.Transparent;
+            this.divar4.Location = new System.Drawing.Point(385, 50);
+            this.divar4.Name = "divar4";
+            this.divar4.Size = new System.Drawing.Size(17, 352);
+            this.divar4.TabIndex = 2;
+            // 
+            // divar2
+            // 
+            this.divar2.BackColor = System.Drawing.Color.Transparent;
+            this.divar2.Location = new System.Drawing.Point(49, 369);
+            this.divar2.Name = "divar2";
+            this.divar2.Size = new System.Drawing.Size(320, 33);
+            this.divar2.TabIndex = 3;
+            // 
+            // divar13
+            // 
+            this.divar13.BackColor = System.Drawing.Color.Transparent;
+            this.divar13.Location = new System.Drawing.Point(85, 151);
+            this.divar13.Name = "divar13";
+            this.divar13.Size = new System.Drawing.Size(31, 104);
+            this.divar13.TabIndex = 4;
+            // 
+            // divar9
+            // 
+            this.divar9.BackColor = System.Drawing.Color.Transparent;
+            this.divar9.Location = new System.Drawing.Point(264, 186);
+            this.divar9.Name = "divar9";
+            this.divar9.Size = new System.Drawing.Size(31, 104);
+            this.divar9.TabIndex = 5;
+            // 
+            // divar8
+            // 
+            this.divar8.BackColor = System.Drawing.Color.Transparent;
+            this.divar8.Location = new System.Drawing.Point(229, 124);
+            this.divar8.Name = "divar8";
+            this.divar8.Size = new System.Drawing.Size(31, 60);
+            this.divar8.TabIndex = 6;
+            // 
+            // divar5
+            // 
+            this.divar5.BackColor = System.Drawing.Color.Transparent;
+            this.divar5.Location = new System.Drawing.Point(157, 73);
+            this.divar5.Name = "divar5";
+            this.divar5.Size = new System.Drawing.Size(31, 39);
+            this.divar5.TabIndex = 7;
+            // 
+            // divar14
+            // 
+            this.divar14.BackColor = System.Drawing.Color.Transparent;
+            this.divar14.Location = new System.Drawing.Point(157, 151);
+            this.divar14.Name = "divar14";
+            this.divar14.Size = new System.Drawing.Size(31, 33);
+            this.divar14.TabIndex = 8;
+            // 
+            // divar15
+            // 
+            this.divar15.BackColor = System.Drawing.Color.Transparent;
+            this.divar15.Location = new System.Drawing.Point(157, 222);
+            this.divar15.Name = "divar15";
+            this.divar15.Size = new System.Drawing.Size(31, 33);
+            this.divar15.TabIndex = 9;
+            // 
+            // divar10
+            // 
+            this.divar10.BackColor = System.Drawing.Color.Transparent;
+            this.divar10.Location = new System.Drawing.Point(194, 257);
+            this.divar10.Name = "divar10";
+            this.divar10.Size = new System.Drawing.Size(137, 33);
+            this.divar10.TabIndex = 10;
+            // 
+            // divar6
+            // 
+            this.divar6.BackColor = System.Drawing.Color.Transparent;
+            this.divar6.Location = new System.Drawing.Point(264, 124);
+            this.divar6.Name = "divar6";
+            this.divar6.Size = new System.Drawing.Size(31, 23);
+            this.divar6.TabIndex = 11;
+            // 
+            // divar7
+            // 
+            this.divar7.BackColor = System.Drawing.Color.Transparent;
+            this.divar7.Location = new System.Drawing.Point(300, 151);
+            this.divar7.Name = "divar7";
+            this.divar7.Size = new System.Drawing.Size(21, 33);
+            this.divar7.TabIndex = 12;
+            // 
+            // divar12
+            // 
+            this.divar12.BackColor = System.Drawing.Color.Transparent;
+            this.divar12.Location = new System.Drawing.Point(121, 292);
+            this.divar12.Name = "divar12";
+            this.divar12.Size = new System.Drawing.Size(67, 33);
+            this.divar12.TabIndex = 13;
+            // 
+            // divar11
+            // 
+            this.divar11.BackColor = System.Drawing.Color.Transparent;
+            this.divar11.Location = new System.Drawing.Point(302, 339);
+            this.divar11.Name = "divar11";
+            this.divar11.Size = new System.Drawing.Size(29, 21);
+            this.divar11.TabIndex = 14;
+            // 
+            // irani2
+            // 
+            this.irani2.BackColor = System.Drawing.Color.Transparent;
+            this.irani2.Location = new System.Drawing.Point(194, 222);
+            this.irani2.Name = "irani2";
+            this.irani2.Size = new System.Drawing.Size(31, 33);
+            this.irani2.TabIndex = 15;
+            // 
+            // irani3
+            // 
+            this.irani3.BackColor = System.Drawing.Color.Transparent;
+            this.irani3.Location = new System.Drawing.Point(300, 186);
+            this.irani3.Name = "irani3";
+            this.irani3.Size = new System.Drawing.Size(31, 33);
+            this.irani3.TabIndex = 16;
+            // 
+            // irani1
+            // 
+            this.irani1.BackColor = System.Drawing.Color.Transparent;
+            this.irani1.Location = new System.Drawing.Point(84, 257);
+            this.irani1.Name = "irani1";
+            this.irani1.Size = new System.Drawing.Size(31, 33);
+            this.irani1.TabIndex = 17;
+            // 
+            // irani4
+            // 
+            this.irani4.BackColor = System.Drawing.Color.Transparent;
+            this.irani4.Location = new System.Drawing.Point(229, 327);
+            this.irani4.Name = "irani4";
+            this.irani4.Size = new System.Drawing.Size(31, 33);
+            this.irani4.TabIndex = 18;
+            // 
+            // kh1
+            // 
+            this.kh1.BackColor = System.Drawing.Color.Transparent;
+            this.kh1.Location = new System.Drawing.Point(337, 339);
+            this.kh1.Name = "kh1";
+            this.kh1.Size = new System.Drawing.Size(31, 21);
+            this.kh1.TabIndex = 19;
+            // 
+            // kh2
+            // 
+            this.kh2.BackColor = System.Drawing.Color.Transparent;
+            this.kh2.Location = new System.Drawing.Point(338, 79);
+            this.kh2.Name = "kh2";
+            this.kh2.Size = new System.Drawing.Size(31, 33);
+            this.kh2.TabIndex = 20;
+            // 
+            // kh3
+            // 
+            this.kh3.BackColor = System.Drawing.Color.Transparent;
+            this.kh3.Location = new System.Drawing.Point(49, 79);
+            this.kh3.Name = "kh3";
+            this.kh3.Size = new System.Drawing.Size(31, 33);
+            this.kh3.TabIndex = 21;
+            // 
+            // kh4
+            // 
+            this.kh4.BackColor = System.Drawing.Color.Transparent;
+            this.kh4.Location = new System.Drawing.Point(49, 327);
+            this.kh4.Name = "kh4";
+            this.kh4.Size = new System.Drawing.Size(31, 33);
+            this.kh4.TabIndex = 22;
+            // 
+            // khar2
+            // 
+            this.khar2.BackColor = System.Drawing.Color.Transparent;
+            this.khar2.Location = new System.Drawing.Point(120, 151);
+            this.khar2.Name = "khar2";
+            this.khar2.Size = new System.Drawing.Size(31, 33);
+            this.khar2.TabIndex = 23;
+            // 
+            // khar1
+            // 
+            this.khar1.BackColor = System.Drawing.Color.Transparent;
+            this.khar1.Location = new System.Drawing.Point(157, 255);
+            this.khar1.Name = "khar1";
+            this.khar1.Size = new System.Drawing.Size(31, 33);
+            this.khar1.TabIndex = 25;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(93, 413);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 413);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 31);
+            this.button2.TabIndex = 27;
+            this.button2.Text = "finish";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.Location = new System.Drawing.Point(363, 413);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(39, 31);
+            this.button5.TabIndex = 30;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.Location = new System.Drawing.Point(318, 413);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(39, 31);
+            this.button6.TabIndex = 31;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Enabled = false;
+            this.button7.Image = ((System.Drawing.Image)(resources.GetObject("button7.Image")));
+            this.button7.Location = new System.Drawing.Point(273, 413);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(39, 31);
+            this.button7.TabIndex = 32;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Enabled = false;
+            this.button8.Image = ((System.Drawing.Image)(resources.GetObject("button8.Image")));
+            this.button8.Location = new System.Drawing.Point(228, 413);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(39, 31);
+            this.button8.TabIndex = 33;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Button8_Click);
+            // 
+            // lablel1
+            // 
+            this.lablel1.BackColor = System.Drawing.Color.Transparent;
+            this.lablel1.Image = ((System.Drawing.Image)(resources.GetObject("lablel1.Image")));
+            this.lablel1.Location = new System.Drawing.Point(157, 327);
+            this.lablel1.Name = "lablel1";
+            this.lablel1.Size = new System.Drawing.Size(31, 33);
+            this.lablel1.TabIndex = 34;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(414, 448);
+            this.Controls.Add(this.lablel1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.khar1);
+            this.Controls.Add(this.khar2);
+            this.Controls.Add(this.kh4);
+            this.Controls.Add(this.kh3);
+            this.Controls.Add(this.kh2);
+            this.Controls.Add(this.kh1);
+            this.Controls.Add(this.irani4);
+            this.Controls.Add(this.irani1);
+            this.Controls.Add(this.irani3);
+            this.Controls.Add(this.irani2);
+            this.Controls.Add(this.divar11);
+            this.Controls.Add(this.divar12);
+            this.Controls.Add(this.divar7);
+            this.Controls.Add(this.divar6);
+            this.Controls.Add(this.divar10);
+            this.Controls.Add(this.divar15);
+            this.Controls.Add(this.divar14);
+            this.Controls.Add(this.divar5);
+            this.Controls.Add(this.divar8);
+            this.Controls.Add(this.divar9);
+            this.Controls.Add(this.divar13);
+            this.Controls.Add(this.divar2);
+            this.Controls.Add(this.divar4);
+            this.Controls.Add(this.divar3);
+            this.Controls.Add(this.divar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label divar1;
+        private System.Windows.Forms.Label divar3;
+        private System.Windows.Forms.Label divar4;
+        private System.Windows.Forms.Label divar2;
+        private System.Windows.Forms.Label divar13;
+        private System.Windows.Forms.Label divar9;
+        private System.Windows.Forms.Label divar8;
+        private System.Windows.Forms.Label divar5;
+        private System.Windows.Forms.Label divar14;
+        private System.Windows.Forms.Label divar15;
+        private System.Windows.Forms.Label divar10;
+        private System.Windows.Forms.Label divar6;
+        private System.Windows.Forms.Label divar7;
+        private System.Windows.Forms.Label divar12;
+        private System.Windows.Forms.Label divar11;
+        private System.Windows.Forms.Label irani2;
+        private System.Windows.Forms.Label irani3;
+        private System.Windows.Forms.Label irani1;
+        private System.Windows.Forms.Label irani4;
+        private System.Windows.Forms.Label kh1;
+        private System.Windows.Forms.Label kh2;
+        private System.Windows.Forms.Label kh3;
+        private System.Windows.Forms.Label kh4;
+        private System.Windows.Forms.Label khar2;
+        private System.Windows.Forms.Label khar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label lablel1;
+    }
+}
+
---
+++ BAZI 1/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BAZI_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int s;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            s = 0;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+            button8.Enabled = true;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("your score is = " + s.ToString());
+            this.Close();
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            int old = lablel1.Left;
+            lablel1.Left += lablel1.Width + 6;
+
+            if (lablel1.Bounds.IntersectsWith(irani1.Bounds) || lablel1.Bounds.IntersectsWith(irani2.Bounds) || lablel1.Bounds.IntersectsWith(irani3.Bounds) || lablel1.Bounds.IntersectsWith(irani4.Bounds))
+            {
+                MessageBox.Show("few score added ");
+                s = +5;
+            }
+            if (lablel1.Bounds.IntersectsWith(kh1.Bounds) || lablel1.Bounds.IntersectsWith(kh2.Bounds) || lablel1.Bounds.IntersectsWith(kh3.Bounds) || lablel1.Bounds.IntersectsWith(kh4.Bounds) || lablel1.Bounds.IntersectsWith(khar1.Bounds) || lablel1.Bounds.IntersectsWith(khar2.Bounds))
+            {
+                MessageBox.Show(" you lost score ");
+                s--;
+            }
+            if (lablel1.Bounds.IntersectsWith(divar1.Bounds) || lablel1.Bounds.IntersectsWith(divar2.Bounds) || lablel1.Bounds.IntersectsWith(divar3.Bounds) || lablel1.Bounds.IntersectsWith(divar4.Bounds) || lablel1.Bounds.IntersectsWith(divar5.Bounds) || lablel1.Bounds.IntersectsWith(divar6.Bounds) || lablel1.Bounds.IntersectsWith(divar7.Bounds) || lablel1.Bounds.IntersectsWith(divar8.Bounds) || lablel1.Bounds.IntersectsWith(divar9.Bounds) || lablel1.Bounds.IntersectsWith(divar10.Bounds) || lablel1.Bounds.IntersectsWith(divar11.Bounds) || lablel1.Bounds.IntersectsWith(divar12.Bounds) || lablel1.Bounds.IntersectsWith(divar13.Bounds) || lablel1.Bounds.IntersectsWith(divar14.Bounds) || lablel1.Bounds.IntersectsWith(divar15.Bounds))
+                lablel1.Left = old;
+
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            int old = lablel1.Left;
+            lablel1.Left -= lablel1.Width + 6;
+            if (lablel1.Bounds.IntersectsWith(irani1.Bounds) || lablel1.Bounds.IntersectsWith(irani2.Bounds) || lablel1.Bounds.IntersectsWith(irani3.Bounds) || lablel1.Bounds.IntersectsWith(irani4.Bounds))
+            {
+                MessageBox.Show("few score added ");
+                s = +5;
+            }
+            if (lablel1.Bounds.IntersectsWith(kh1.Bounds) || lablel1.Bounds.IntersectsWith(kh2.Bounds) || lablel1.Bounds.IntersectsWith(kh3.Bounds) || lablel1.Bounds.IntersectsWith(kh4.Bounds) || lablel1.Bounds.IntersectsWith(khar1.Bounds) || lablel1.Bounds.IntersectsWith(khar2.Bounds))
+            {
+                MessageBox.Show(" you lost score ");
+                s--;
+            }
+            if (lablel1.Bounds.IntersectsWith(divar1.Bounds) || lablel1.Bounds.IntersectsWith(divar2.Bounds) || lablel1.Bounds.IntersectsWith(divar3.Bounds) || lablel1.Bounds.IntersectsWith(divar4.Bounds) || lablel1.Bounds.IntersectsWith(divar5.Bounds) || lablel1.Bounds.IntersectsWith(divar6.Bounds) || lablel1.Bounds.IntersectsWith(divar7.Bounds) || lablel1.Bounds.IntersectsWith(divar8.Bounds) || lablel1.Bounds.IntersectsWith(divar9.Bounds) || lablel1.Bounds.IntersectsWith(divar10.Bounds) || lablel1.Bounds.IntersectsWith(divar11.Bounds) || lablel1.Bounds.IntersectsWith(divar12.Bounds) || lablel1.Bounds.IntersectsWith(divar13.Bounds) || lablel1.Bounds.IntersectsWith(divar14.Bounds) || lablel1.Bounds.IntersectsWith(divar15.Bounds))
+
+                lablel1.Left = old;
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            int old = lablel1.Top;
+            lablel1.Top -= lablel1.Height + 2;
+            if (lablel1.Bounds.IntersectsWith(irani1.Bounds) || lablel1.Bounds.IntersectsWith(irani2.Bounds) || lablel1.Bounds.IntersectsWith(irani3.Bounds) || lablel1.Bounds.IntersectsWith(irani4.Bounds))
+            {
+                MessageBox.Show("few score added ");
+                s = +5;
+            }
+            if (lablel1.Bounds.IntersectsWith(kh1.Bounds) || lablel1.Bounds.IntersectsWith(kh2.Bounds) || lablel1.Bounds.IntersectsWith(kh3.Bounds) || lablel1.Bounds.IntersectsWith(kh4.Bounds) || lablel1.Bounds.IntersectsWith(khar1.Bounds) || lablel1.Bounds.IntersectsWith(khar2.Bounds))
+            {
+                MessageBox.Show(" you lost score ");
+                s--;
+            }
+            if (lablel1.Bounds.IntersectsWith(divar1.Bounds) || lablel1.Bounds.IntersectsWith(divar2.Bounds) || lablel1.Bounds.IntersectsWith(divar3.Bounds) || lablel1.Bounds.IntersectsWith(divar4.Bounds) || lablel1.Bounds.IntersectsWith(divar5.Bounds) || lablel1.Bounds.IntersectsWith(divar6.Bounds) || lablel1.Bounds.IntersectsWith(divar7.Bounds) || lablel1.Bounds.IntersectsWith(divar8.Bounds) || lablel1.Bounds.IntersectsWith(divar9.Bounds) || lablel1.Bounds.IntersectsWith(divar10.Bounds) || lablel1.Bounds.IntersectsWith(divar11.Bounds) || lablel1.Bounds.IntersectsWith(divar12.Bounds) || lablel1.Bounds.IntersectsWith(divar13.Bounds) || lablel1.Bounds.IntersectsWith(divar14.Bounds) || lablel1.Bounds.IntersectsWith(divar15.Bounds))
+
+                lablel1.Top = old;
+        }
+
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            int old = lablel1.Top;
+            lablel1.Top += lablel1.Height + 2;
+            if (lablel1.Bounds.IntersectsWith(irani1.Bounds) || lablel1.Bounds.IntersectsWith(irani2.Bounds) || lablel1.Bounds.IntersectsWith(irani3.Bounds) || lablel1.Bounds.IntersectsWith(irani4.Bounds))
+            {
+                MessageBox.Show("few score added ");
+                s = +5;
+            }
+            if (lablel1.Bounds.IntersectsWith(kh1.Bounds) || lablel1.Bounds.IntersectsWith(kh2.Bounds) || lablel1.Bounds.IntersectsWith(kh3.Bounds) || lablel1.Bounds.IntersectsWith(kh4.Bounds) || lablel1.Bounds.IntersectsWith(khar1.Bounds) || lablel1.Bounds.IntersectsWith(khar2.Bounds))
+            {
+                MessageBox.Show(" you lost score ");
+                s--;
+            }
+            if (lablel1.Bounds.IntersectsWith(divar1.Bounds) || lablel1.Bounds.IntersectsWith(divar2.Bounds) || lablel1.Bounds.IntersectsWith(divar3.Bounds) || lablel1.Bounds.IntersectsWith(divar4.Bounds) || lablel1.Bounds.IntersectsWith(divar5.Bounds) || lablel1.Bounds.IntersectsWith(divar6.Bounds) || lablel1.Bounds.IntersectsWith(divar7.Bounds) || lablel1.Bounds.IntersectsWith(divar8.Bounds) || lablel1.Bounds.IntersectsWith(divar9.Bounds) || lablel1.Bounds.IntersectsWith(divar10.Bounds) || lablel1.Bounds.IntersectsWith(divar11.Bounds) || lablel1.Bounds.IntersectsWith(divar12.Bounds) || lablel1.Bounds.IntersectsWith(divar13.Bounds) || lablel1.Bounds.IntersectsWith(divar14.Bounds) || lablel1.Bounds.IntersectsWith(divar15.Bounds))
+
+                lablel1.Top = old;
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
